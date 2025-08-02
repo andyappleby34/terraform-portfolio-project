705ca44e4c3852139144b5df0a6a3519c221b7ec
Andy Appleby <aappleby@MacBookPro.localdomain>
2025-08-02 16:39:37 -0500

    tf update 1

--- nextjs-blog/terraform-js/main.tf
+++ nextjs-blog/terraform-js/main.tf
@@ -12,7 +12,7 @@ resource "aws_s3_bucket_ownership_controls" "nextjs_bucket_ownership_controls" {
   bucket = aws_s3_bucklet.nextjs_bucket.id
 
   rule {
-    object_ownership = "BucketOwnerPrefferred"
+    object_ownership = "BucketOwnerPreferred"
   }
 }
 
@@ -29,7 +29,7 @@ resource "aws_s3_bucket_public_access_block" "nextjs_bucket_public_block" {
 # Bucket ACL
 resource "aws_s3_bucket_acl" "nextjs_bucket_acl" {
     depends_on = [ 
-        aws_s3_bucket_ownership_controls.nextjs,
+        aws_s3_bucket_ownership_controls.nextjs_bucket_ownership_controls,
         aws_s3_bucket_aws_s3_bucket_public_access_block.nextjs_bucket_public_block
      ]
     bucket = aws_s3_bucket.nextjs_bucket.id
@@ -39,7 +39,7 @@ resource "aws_s3_bucket_acl" "nextjs_bucket_acl" {
 # Bucket Policy
 resource "aws_s3_bucket_policy" "nextjs_bucket_policy" {
     bucket = aws_s3_bucket.nextjs_bucket.id
-    policy = jsondecode(({
+    policy = jsonencode ({
         version = "2012-10-17"
         Statement = [
             {
@@ -50,7 +50,54 @@ resource "aws_s3_bucket_policy" "nextjs_bucket_policy" {
                 Resource = "${aws_s3_bucket.nextjs_bucket.arn}/*"
             }
         ]
-    }))
-      
-    }]
+    })
+}
+
+# Origin Access Identity
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+    comment = "OAI for Next.JS portfolio site"
+}
+
+# Cloudfront Distribution
+resource = "aws_cloudfront_distribution" "nextjs_distribution" {
+
+    origin {
+        domain_name = aws_s3_bucket.nextjs_bucket.bucket_regional_domain_name,
+        origin_id = "s3-nextjs-portfolio-bucket"
+
+        s3_origin_config {
+            origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
+        }   
+    }
+    enabled = true
+    is_ipv6_enabled = true
+    comment = "Next.js portfolio site"
+    default_root_object = "index.html"
+
+    default_cache_behavior {
+        allowed_methods = [ "GET", "HEAD", "OPTIONS"]
+        cached_methods = ["GET", "HEAD"]
+        target_origin_id = "s3-nextjs-portfoilo-bucket"
+
+        forwarded_values {
+            query_string = false
+            cookies {
+                forward = "none"
+            }
+        }
+    viewer_protocol_policy = "redirect-to-https"
+        min_ttl = 0
+        default_ttl = 3600
+        max_ttl = 86400
+    }
+        
+    restrictions {
+        geo_restriction {
+            restriction_type = "none"
+        }
+    }
+    viewer_certificate {
+        cloudfront_default_certificate = true
+    }
+
 }
